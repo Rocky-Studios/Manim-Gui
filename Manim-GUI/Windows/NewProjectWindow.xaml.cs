1724055c672b3ab17fff5c77bb9ad78d811bdfa9
RS-GoogleCoder <gergo.kaszas@outlook.com>
2024-07-04 07:17:03 +1000

    NewProjectWindow

---
+++ Manim-GUI/Windows/NewProjectWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Manim_GUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for NewProjectWindow.xaml
+    /// </summary>
+    public partial class NewProjectWindow : Window
+    {
+        string projectName;
+        string projectLocation;
+        public NewProjectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProjectLocationButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Configure save file dialog box
+            var dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.FileName = projectName; // Default file name
+            dialog.DefaultExt = ".mgui"; // Default file extension
+            dialog.Filter = "Manim GUI Project File (.mgui)|*.mgui"; // Filter files by extension
+
+            // Show save file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                projectLocation = dialog.FileName;
+                string[] pathAsArray = projectLocation.Split('\\');
+                (sender as Button).Content = "Choose (" + pathAsArray[pathAsArray.Length - 2] + "/" + pathAsArray[pathAsArray.Length-1] +")";
+            }
+
+        }
+
+        private void ProjectNameBoxTextChange(object sender, TextChangedEventArgs e)
+        {
+            projectName = (sender as TextBox).Text;
+        }
+    }
+}
